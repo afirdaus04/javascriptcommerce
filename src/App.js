bbf12ef31f13b1865a1f3b6c4d689298d4cc284e
afirdaus04 <afirdaus.pe@gmail.com>
2022-05-31 22:52:22 +0800

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Products from './components/Products/Products';
+import React from 'react';
+
+// import Products from './components/Products/Products'
+// import Navbar from './components/Navbar/Navbar'
+// below code is shortform to import many items
+
+import { Products, Navbar } from './components';
 
 const App = () => {
   return (
     <div>
+        <Navbar />
         <Products />
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 //create variable of products
 const products = [
@@ -12,9 +13,13 @@ const products = [
 
 
 const Products = () => {
+const classes = useStyles();
+
+
     return (
-        <main>
-        <Grid container justify="center" spacing={4}>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <Product product={product} />
